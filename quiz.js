ffd0e78caa1a1bb3487188cd6f2ad8b696a28acc
Mohammed Nuredin Saud <153536948+mammeecoding@users.noreply.github.com>
2024-10-08 21:48:51 +0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,16 @@
+ function  checkAnswer() {
+  const correctAnswer = '4';
+  const userAnswer =  document.querySelector('name="quiz"').value;
+  const correct = document.getElementById('#feedback');
+  if(userAnswer == correctAnswer){
+    correct.textContent = 'Correct! Well done.';
+  }else{
+    correct.textContent = "That's incorrect. Try again!";
+  }
+  
+  }
+
+  const submit = document.getElementById('submit-answer');
+  submit.addEventListener('click', function(checkAnswer) {
+    alert(checkAnswer);
+  })
